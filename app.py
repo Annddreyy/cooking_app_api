e715814c0ea616a191734c3052b098306892d931
Annddreyy <andreymail22112006@gmail.com>
2024-10-11 18:12:10 +0300

    Add recipe types

--- app.py
+++ app.py
@@ -61,5 +61,27 @@ def get_recipes():
     return jsonify(recipes_json)
 
 
+@app.route('/api/v1/recipe_types')
+def get_recipe_types():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM recipe_type')
+
+    recipe_types = cur.fetchall()
+
+    recipe_types_json = []
+    for recipe_type in recipe_types:
+        recipe_types_json.append(
+            {
+                'id': recipe_type[0],
+                'title': recipe_type[1],
+                'image_path': recipe_type[2],
+            }
+        )
+
+    return jsonify(recipe_types_json)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=1234)
