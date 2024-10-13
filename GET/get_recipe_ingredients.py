8a903a95873e79a0fdb991489706f7ecc384bd77
Annddreyy <andreymail22112006@gmail.com>
2024-10-13 15:08:16 +0300

    Add get recipe ingredients

---
+++ GET/get_recipe_ingredients.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, jsonify
+
+from db import get_connection
+
+get_recipe_ingredients_blueprint = Blueprint('get_recipe_ingredients', __name__)
+
+@get_recipe_ingredients_blueprint.route('/api/v1/recipe_ingredients/<int:recipe_id>')
+def get_recipe_ingredients(recipe_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(f'SELECT ingredient_id FROM recipe_ingredient WHERE recipe_id={recipe_id}')
+
+    ingredients_id = cur.fetchall()
+
+    ingredient_titles_json = []
+
+    for ingredient_id in ingredients_id:
+        cur.execute(f'SELECT title, count FROM ingredient WHERE ingredient_id={ingredient_id[0]}')
+
+        ingredient = cur.fetchone()
+
+        ingredient_titles_json.append(
+            {
+                'id': ingredient_id[0],
+                'title': ingredient[0],
+                'count': ingredient[1]
+            }
+        )
+
+    return jsonify(ingredient_titles_json)
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from GET.get_clients import get_clients_blueprint
+from GET.get_recipe_ingredients import get_recipe_ingredients_blueprint
 from GET.get_recipe_types import get_recipe_types_blueprint
 from GET.get_recipes import get_recipes_blueprint
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(get_clients_blueprint)
 app.register_blueprint(get_recipes_blueprint)
 app.register_blueprint(get_recipe_types_blueprint)
+app.register_blueprint(get_recipe_ingredients_blueprint)
 
 
 if __name__ == '__main__':
