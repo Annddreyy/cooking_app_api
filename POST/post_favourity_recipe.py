8394ff10fbb8b5f93a94abde357bea1b58a04398
Annddreyy <andreymail22112006@gmail.com>
2024-10-17 15:31:10 +0300

    Add post favourity recipe

---
+++ POST/post_favourity_recipe.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, jsonify, request
+
+from db import get_connection
+
+post_favourity_recipe_blueprint = Blueprint('post_favourity_recipe', __name__)
+
+@post_favourity_recipe_blueprint.route('/api/v1/favourite_recipes/<int:client_id>', methods=['POST'])
+def add_favourity_recipe(client_id):
+    global conn, cur
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        data = request.json
+
+        client_id = data['client_id']
+        recipe_id = data['recipe_id']
+
+        cur.execute(f'INSERT INTO favourity_recipe(client_id, recipe_id) VALUES ({client_id}, {recipe_id})')
+
+        conn.commit()
+
+        return jsonify({'message': 'ok'})
+    except:
+        return jsonify({'message': 'error'})
+    finally:
+        cur.close()
+        conn.close()
